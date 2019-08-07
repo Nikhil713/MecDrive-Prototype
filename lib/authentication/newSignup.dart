aa5dad21e3e496bbf49077ad7fa9e24d850b379f
JoyalAJohney <joyalajohny@gmail.com>
2019-08-07 23:01:03 +0530

    new sign up page created

--- lib/authentication/newSignup.dart
+++ lib/authentication/newSignup.dart
@@ -7,8 +7,13 @@ class SignUp extends StatefulWidget {
 
 class _SignUpState extends State<SignUp> {
   final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+
+  // Form field variables
+  String name = "";
+  String phone = "";
   String branch = "CSA";
   String year = '1';
+  int carpool;
 
   @override
   Widget build(BuildContext context) {
@@ -31,9 +36,9 @@ class _SignUpState extends State<SignUp> {
               SizedBox(
                 height: 140.0,
               ),
+              // Sign Up container
               Container(
-                // Container design
-                padding: EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 0.0),
+                padding: EdgeInsets.fromLTRB(30.0, 10.0, 30.0, 10.0),
                 width: 300.0,
                 height: 420.0,
                 decoration: BoxDecoration(
@@ -53,19 +58,51 @@ class _SignUpState extends State<SignUp> {
                 child: Form(
                   key: formKey,
                   autovalidate: true,
-                  child: ListView(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: <Widget>[
                       // UserName
                       TextFormField(
-                        decoration: InputDecoration(labelText: "Name"),
+                        decoration: InputDecoration(
+                          labelText: "Name",
+                          icon: Icon(Icons.account_circle)
+                        ),
+                        keyboardType: TextInputType.text,
+                        style: TextStyle(
+                          fontFamily: "Poppins"
+                        ),
+                        validator: (value) {
+                          if (value.isEmpty) return "Name field is required";
+                        },
+                        onSaved: (val) {
+                          setState(() {
+                            name = val;
+                          });
+                        },
+                      ),
+                      // Phone
+                      TextFormField(
+                        decoration: InputDecoration(
+                          labelText: "Phone",
+                          icon: Icon(Icons.phone)
+                        ),
+                        keyboardType: TextInputType.phone,
+                        validator: (value) {
+                          if (value.isEmpty) return "Phone no is required";
+                        },
+                        onSaved: (val) {
+                          setState(() {
+                            phone = val;
+                          });
+                        },
                       ),
                       Padding(
-                        padding: EdgeInsets.all(13.0),
+                        padding: EdgeInsets.all(10.0),
                       ),
                       // Branch
                       Row(
                         children: <Widget>[
-                          Text("Branch: ", style: TextStyle(fontSize: 18.0)),
+                          Text("Branch: ", style: TextStyle(fontSize: 17.0)),
                           Padding(padding: EdgeInsets.all(5.0)),
                           DropdownButton<String>(
                             value: branch,
@@ -90,7 +127,7 @@ class _SignUpState extends State<SignUp> {
                           ),
                           Padding(padding: EdgeInsets.all(10.0)),
                           // Year
-                          Text("Year: ", style: TextStyle(fontSize: 18.0)),
+                          Text("Year: ", style: TextStyle(fontSize: 17.0)),
                           Padding(padding: EdgeInsets.all(5.0)),
                           DropdownButton<String>(
                             value: year,
@@ -103,12 +140,66 @@ class _SignUpState extends State<SignUp> {
                             }).toList(),
                             onChanged: (String value) {
                               setState(() {
-                                branch = value;
+                                year = value;
                               });
                             },
                           )
                         ],
                       ),
+                      Padding(padding: EdgeInsets.all(10.0)),
+                      // Carpool
+                      Row(
+                        children: <Widget>[
+                          Text(
+                            "Do you have a vehicle to pool?",
+                            style: TextStyle(fontSize: 17.0),
+                          ),
+                        ],
+                      ),
+                      Padding(padding: EdgeInsets.all(2.0)),
+                      // RadioButton
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: <Widget>[
+                          Text("Yes:", style: TextStyle(fontSize: 17.0)),
+                          Radio(
+                            activeColor: Colors.green,
+                            value: 1,
+                            groupValue: carpool,
+                            onChanged: (int val) {
+                              setState(() {
+                                carpool = val;
+                              });
+                            },
+                          ),
+                          Text("No:", style: TextStyle(fontSize: 17.0)),
+                          Radio(
+                            activeColor: Colors.green,
+                            value: 0,
+                            groupValue: carpool,
+                            onChanged: (int val) {
+                              setState(() {
+                                carpool = val;
+                              });
+                            },
+                          )
+                        ],
+                      ),
+                      Padding(padding: EdgeInsets.all(10.0)),
+                      // Submit Button
+                      Container(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            RaisedButton(
+                              color: Colors.green,
+                              child: Text("Register",),
+                              textColor: Colors.white,
+                              onPressed: (){},
+                            )
+                          ],
+                        ),
+                      )
                     ],
                   ),
                 ),
