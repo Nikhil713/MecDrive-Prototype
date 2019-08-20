168cd629de9fcef0fdad39370fe3591d314e8880
JoyalAJohney <joyalajohny@gmail.com>
2019-08-20 22:34:18 +0530

    time picker added

--- lib/Widgets/HomeScreen/findaRide.dart
+++ lib/Widgets/HomeScreen/findaRide.dart
@@ -9,6 +9,21 @@ class _FindaRideState extends State<FindaRide> {
   // Destination
   String destination = "";
 
+  TimeOfDay pickedTime = TimeOfDay.now();
+
+  Future<Null> _selectTime(BuildContext context) async {
+    final TimeOfDay response = await showTimePicker(
+      context: context,
+      initialTime: pickedTime,
+    );
+    if (response != null && response != pickedTime) {
+      setState(() {
+        pickedTime = response;
+        print(pickedTime);
+      });
+    }
+  } 
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -32,10 +47,12 @@ class _FindaRideState extends State<FindaRide> {
         ),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: <Widget>[
             // Starting Location
             Container(
               decoration: BoxDecoration(
+                  color: Colors.redAccent,
                   border: Border.all(color: Colors.black12),
                   borderRadius: BorderRadius.circular(8.0)),
               child: ListTile(
@@ -53,16 +70,15 @@ class _FindaRideState extends State<FindaRide> {
             Container(
               padding: EdgeInsets.all(10.0),
               decoration: BoxDecoration(
-                  border: Border.all(color: Colors.black12),
-                  borderRadius: BorderRadius.circular(8.0)),
+                // color: Colors.blueAccent,
+                border: Border.all(color: Colors.black12),
+                borderRadius: BorderRadius.circular(8.0)),
               child: ListTile(
                 leading: Icon(Icons.location_on),
                 title: TextField(
                   decoration: InputDecoration(
+                    contentPadding: EdgeInsets.all(0.0),
                     hintText: "Destination",
-                    enabledBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(color: Colors.black12),
-                    ),
                     focusedBorder: UnderlineInputBorder(
                       borderSide: BorderSide(color: Colors.black12),
                     ),
@@ -74,6 +90,43 @@ class _FindaRideState extends State<FindaRide> {
                   },
                 ),
               ),
+            ),
+            // Time
+            RaisedButton(
+              child: Text("Time"),
+              onPressed: () => _selectTime(context),
+            ),
+            // Confirm button
+            GestureDetector(
+              child: Container(
+                padding: EdgeInsets.all(10.0),
+                child: Center(
+                  child: Text(
+                    "Find A Ride",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18.0
+                    ),
+                  ),
+                ),
+                decoration: BoxDecoration(
+                  color: Colors.green,
+                  borderRadius: BorderRadius.circular(10.0)
+                ),
+              ),
+              onTap: (){
+                showBottomSheet(
+                  context: context,
+                  builder: (context) => Container(
+                    height: 250,
+                    color: Colors.blue,
+                    child: Center(
+                      child: Text("Test"),
+                    ),
+                  )
+                );
+              },
             )
           ],
         ),
