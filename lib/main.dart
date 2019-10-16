e1a8327b87c94cbfe7e9c2c6267ae30f8b94134b
JoyalAJohney <joyalajohny@gmail.com>
2019-10-16 11:25:13 +0530

    Splash screen height fixed

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:mec_drive/authentication/httpRequest.dart';
-import 'package:mec_drive/authentication/newSignup.dart';
-import 'package:mec_drive/googleAuth/googleSignIn.dart';
-import 'HomeScreen/homeScreen.dart';
-import 'driverlist/driverList.dart';
-import 'splashscreen/splashscreen.dart';
-import './profile_page/profile.dart';
-import './authentication/login.dart';
+import 'package:mec_drive/splashscreen/splashscreen.dart';
+
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -14,12 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "carpool",
-      home:googleSignIn(),
-      // home: ProfilePage(),
-      // home: Http(), 
-      // home: DriversListPage(),
-      // home: Loading(),
-      // home: Loginpage(),
+      home: SplashScreen(),
     );
   }
 }
