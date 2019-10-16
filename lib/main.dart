013a9b63e3aca39ee068c441b678512513757f7c
JoyalAJohney <joyalajohny@gmail.com>
2019-10-16 09:18:46 +0530

    Google authentication fixed

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:mec_drive/authentication/httpRequest.dart';
-import 'package:mec_drive/authentication/newSignup.dart';
-import 'HomeScreen/homeScreen.dart';
-import 'driverlist/driverList.dart';
-import 'splashscreen/splashscreen.dart';
+import 'package:mec_drive/authentication/googleAuth.dart';
+//import 'package:mec_drive/authentication/httpRequest.dart';
+//import 'package:mec_drive/authentication/newSignup.dart';
+//import 'HomeScreen/homeScreen.dart';
+//import 'driverlist/driverList.dart';
+//import 'splashscreen/splashscreen.dart';
 import './profile_page/profile.dart';
 
 void main() => runApp(MyApp());
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "carpool",
-      home: ProfilePage(),
+      home: GoogleAuth()
       // home: Http(), 
       // home: DriversListPage(),
       // home: Loading(),
